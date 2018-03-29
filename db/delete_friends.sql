f897d1dc2d380a7c87c2a7b51ee45cedd794a19d
KC Hansen <kc.x.hansen@gmail.com>
2018-03-29 14:28:13 -0600

    friendship

--- db/delete_friends.sql
+++ db/delete_friends.sql
@@ -1,5 +1,3 @@
-update relationships 
-set friendstatus = 1 
+delete from relationships 
 where user_id1 = $1 and user_id2 = $2;
-
 select * from relationships;
--- db/get_friends.sql
+++ db/get_friends.sql
@@ -1,3 +1,1 @@
 select * from users
-join relationships on users.id = relationships.user_id2
-where friendstatus is null;

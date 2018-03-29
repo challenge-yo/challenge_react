07088dc6878f6b764cb1cd40fcfeb7af3651186b
unknown <empey.chad@gmail.com>
2018-03-29 15:41:58 -0600

    Auth/Me security

--- src/views/Friends/FriendsCard/FriendsCard.js
+++ src/views/Friends/FriendsCard/FriendsCard.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import Avatar from 'material-ui/Avatar';
 import { List, ListItem } from 'material-ui/List';
-import Subheader from 'material-ui/Subheader';
-import Divider from 'material-ui/Divider';
 import CommunicationChatBubble from 'material-ui/svg-icons/communication/chat-bubble';
 
 class FriendsCard extends Component {

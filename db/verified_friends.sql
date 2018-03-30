17ed73ac023e7b198e9be649569c67aa0818a8db
unknown <empey.chad@gmail.com>
2018-03-30 14:40:18 -0600

    Fixed confirmed friends

--- db/verified_friends.sql
+++ db/verified_friends.sql
@@ -3,6 +3,12 @@
 -- join relationships on users.facebook_id = relationships.user_id1
 -- where relationships.friendstatus = 1 and relationships.user_id1 = $1 or relationships.user_id2 = $1
 
-select * from relationships
-inner join users on users.facebook_id = relationships.user_id1
-where (user_id1 = $1 or user_id2 = $1) and friendstatus = 1;
+select user_id2 as user_id, u.user_name, u.image, u.score, relationships.friendstatus
+from relationships
+join users u on u.facebook_id = relationships.user_id2
+where user_id2 != $1 and friendstatus = 1
+union
+select user_id1,  u.user_name, u.image, u.score, relationships.friendstatus
+from relationships
+join users u on u.facebook_id = relationships.user_id1
+where user_id1 != $1 and friendstatus = 1

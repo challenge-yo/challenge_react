1aa155e1d53ebdb44c8602e27025020f3fb3e3b0
Brett Johnson <k.bretton.johnson@gmail.com>
2018-04-01 11:39:25 -0600

    f

--- src/components/CategoryCard/CategoryCard.js
+++ src/components/CategoryCard/CategoryCard.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react'
-import {List, ListItem} from 'material-ui/List';
+import {List, ListItem } from 'material-ui/List';
 import Avatar from 'material-ui/Avatar';
 import Divider from 'material-ui/Divider';
 
 class CategoryCard extends Component {
     render() {
         return(
-            <List>
-                <ListItem
-                    rightAvatar={<Avatar src={this.props.icon} />}
-                    primaryText={this.props.name} 
-                />
-                <Divider/>
-            </List>
+                    <List>
+                        <ListItem
+                            rightAvatar={<Avatar src={this.props.icon} />}
+                            primaryText={this.props.name} 
+                        />
+                        <Divider/>
+                    </List>  
         )
     }
 }
--- src/components/FeaturedCard/FeaturedCard.js
+++ src/components/FeaturedCard/FeaturedCard.js
@@ -3,38 +3,53 @@ import { Link } from 'react-router-dom'
 import {Card, CardHeader, CardText} from 'material-ui/Card';
 import RaisedButton from 'material-ui/RaisedButton';
 import Add from 'material-ui/svg-icons/content/add';
+import Divider from 'material-ui/Divider';
+import Avatar from 'material-ui/Avatar';
 
 
 
 class FeaturedCard extends Component {
     render() {
         return(
-        <div>
+        <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
             <Card>
-                <CardHeader title={this.props.name} avatar={this.props.icon}/>
-                <CardText>
+                <CardHeader containerStyle={{display: 'flex', alignItems: 'center'}}>
+                <CardText style={{fontSize: 50}}>
+                {this.props.name} 
+                </CardText>
+                </CardHeader>
+                <Divider />
+                <CardText style={{fontSize: 32}}>
                     Description:
                 </CardText>
-                <CardText>
+                <CardText style={{fontSize: 22}}>
                     {this.props.description}
                 </CardText>
+                <Divider />
                 <CardText>
                     Category: {this.props.category}
                 </CardText>
+                <Divider />
                 <CardText>
                     Difficulty: {this.props.difficulty}
                 </CardText>
+                <Divider />
                 <CardText>
                     Days to Complete: {this.props.time}
                 </CardText>
+                <Divider />
+                <CardText>
+                Badge:
+                <Avatar src={this.props.icon}></Avatar>
+                </CardText>
+                <Divider />
+                <Link to={`/wager/${this.props.id}`}><RaisedButton
+                     label='Accept Challenge'
+                     icon={<Add/>}
+                 /></Link>
             </Card>
-            <Link to={`/wager/${this.props.id}`}><RaisedButton
-            label='Accept Challenge'
-            icon={<Add/>}
-            /></Link>
         </div>    
         )
     }
 }
-
 export default FeaturedCard

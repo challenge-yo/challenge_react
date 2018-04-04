2703305194d77cf858731ebe019f88f631ca8bc6
unknown <empey.chad@gmail.com>
2018-04-04 12:06:51 -0600

    added new test

--- src/views/Friends/FriendsCard/FriendsCard.js
+++ src/views/Friends/FriendsCard/FriendsCard.js
@@ -14,10 +14,12 @@ class FriendsCard extends Component {
         const button = this.props.wager ? <RaisedButton secondary={true} label='Verifier' /> : this.props.status === 'friends' ? <RaisedButton onClick={() => this.props.addFriend( this.props.id )} secondary={true} label='Add Friend' /> : this.props.status === 'confirm' ? <RaisedButton onClick={() => this.props.confirmFriend( this.props.id )} secondary={true} label='Confirm' /> : <RaisedButton onClick={() => this.props.removeFriend( this.props.id )} secondary={true} label='Remove' />
 
         return (
+            <div>
                 <Card>
                     <ListItem style={{ margin: '10px'}} primaryText={ this.props.friend} leftAvatar={<Avatar src={ this.props.icon } />} rightIconButton={ button } />
-                    <img src={ this.props.icon } />
                 </Card>
+                <img src={ this.props.icon } />
+            </div>
         )
     }
 }
